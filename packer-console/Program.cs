d3765e8badecc9ca6609a56a510a4a0911392c25
vladimir.shmidt <vladimir.shmidt@totalobjects.spb.ru>
2018-08-27 16:27:59 +0300

    prettify conversion and plan params check

--- packer-console/Program.cs
+++ packer-console/Program.cs
@@ -11,14 +11,26 @@ namespace console
         {
             var result = Parser.Default.ParseArguments<CompressOptions, DecompressOptions>(args);
             result.WithParsed<DecompressOptions>(Decompress).WithParsed<CompressOptions>(Compress);
+            //check source file exists
+            //check read permissions for source file
+            //check destination file dos not exists
+            //check write permissions
+            //check disk capacity //compress => warn for low disk //decompress => fail low disk
             Console.ReadLine();
         }
 
         private static void Compress(CompressOptions options)
         {
-            var settings = new CompressSettings { ChunkSize = options.Chunksize * 1024 * 1024, PoolSize = options.Poolsize };
+            var settings = new CompressSettings { ChunkSize = ConvertMegabytesToBytes(options), PoolSize = options.Poolsize };
             IStrategy strategy = new CompressStrategy(settings);
             strategy.Work(options.Source, options.Destination);
+        }
+
+        private static int ConvertMegabytesToBytes(CompressOptions options)
+        {
+            var kbytes = options.Chunksize * 1024;
+            var bytes = kbytes * 1024;
+            return bytes;
         }
 
         private static void Decompress(DecompressOptions options)
